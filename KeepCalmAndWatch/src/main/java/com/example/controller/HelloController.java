5e31e858d7eefe10a5a893aecc73e11b02c44236
Simeon Markov <simeon.markov1994@gmail.com>
2016-03-26 19:46:32 +0200

    test controller that gets a single user from users table - test it !

--- KeepCalmAndWatch/src/main/java/com/example/controller/HelloController.java
+++ KeepCalmAndWatch/src/main/java/com/example/controller/HelloController.java
@@ -1,9 +1,14 @@
 package com.example.controller;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.model.User;
+import com.example.model.dao.DBUserDAO;
 
 @Controller
 @RequestMapping(value="/index")
@@ -11,8 +16,15 @@ public class HelloController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String sayHello(Model model) {
-		model.addAttribute("text", "Hi !");
-		return "test";
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		DBUserDAO userJDBCTemplate = 
+			      (DBUserDAO)context.getBean("DBUserDAO");
+		User user = userJDBCTemplate.getUser("nikola");
+	      System.out.print("Channel name : " + user.getChannelName() );
+	      System.out.print(", Email : " + user.getEmail() );
+	      System.out.println(", Registered on : " + user.getRegistrationDate());
+	    model.addAttribute(user);
+	    return "test";
 	}	
 
 }
