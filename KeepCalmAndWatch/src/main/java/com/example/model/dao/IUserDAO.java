34fbde64cd59d0aaa6e16cdf028f9b65c737d0f5
Simeon Markov <simeon.markov1994@gmail.com>
2016-03-26 19:41:46 +0200

    UserDAO interface

---
+++ KeepCalmAndWatch/src/main/java/com/example/model/dao/IUserDAO.java
@@ -0,0 +1,20 @@
+package com.example.model.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.example.model.User;
+
+public interface IUserDAO {
+
+	public void setDataSource(DataSource ds);
+
+	public boolean addUser(User user);
+	
+	User getUser(String username);
+
+	List<User> listUsers() throws SQLException;
+
+}
