4edd594c857b6a0614bc611f6540f4c41bdd2e04
Simeon Markov <simeon.markov1994@gmail.com>
2016-03-27 14:34:16 +0300

    Upload controller working WITH HARDCODED USER "nikola"

--- KeepCalmAndWatch/src/main/java/com/example/controller/UploadController.java
+++ KeepCalmAndWatch/src/main/java/com/example/controller/UploadController.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -8,6 +11,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.model.User;
 import com.example.model.Video;
@@ -30,20 +35,37 @@ public class UploadController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	 public String confirmUpload(@ModelAttribute("SpringWeb") Video video, 
-			   ModelMap model) {
-			      model.addAttribute("name", video.getTitle());
-			      model.addAttribute("name", video.getDescription());
-			      model.addAttribute("name", video.getPath());
-			      model.addAttribute("name", video.getViews());
-			      model.addAttribute("name", video.getLikes());
-			      model.addAttribute("name", video.getDislikes());
-			      model.addAttribute("name", video.getThumbnail());
-			      model.addAttribute("name", video.getUploadDate());
-			      model.addAttribute("name", video.getUploader());
+	 public ModelAndView confirmUpload(ModelMap model, @RequestParam("title") String title, @RequestParam("description") String description, @RequestParam("thumbnail") byte[] thumbnail , @RequestParam("videoPath") String videoPath) {
+			ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+				  ModelAndView mav = new ModelAndView("uploadFinalization");
+				  User user = ((DBUserDAO) context.getBean("DBUserDAO")).getUser("nikola");
+				  mav.addObject("message", "User with channel name " + user.getChannelName() + "uploaded a video with path " +  videoPath + ",title " + title + " and description " + description +  " was sucessfully uploaded!");
+				  DBVideoDAO videoJDBCTemplate = 
+					      (DBVideoDAO)context.getBean("DBVideoDAO");
+				  Video video = new Video();
+				 
+				  
+					video.setTitle(title);
+					video.setDescription(description);
+					video.setPath(videoPath);
+					video.setViews(0);
+					video.setLikes(0);
+					video.setDislikes(0);
+					video.setThumbnail(thumbnail);
+					video.setUploadDate(Date.valueOf(LocalDate.now()));
+					video.setUploader(user); //TODO: how to add the current logged user as an uploader
+					videoJDBCTemplate.addVideo(video);
+//			      model.addAttribute("name", video.getTitle());
+//			      model.addAttribute("name", video.getDescription());
+//			      model.addAttribute("name", video.getPath());
+//			      model.addAttribute("name", video.getViews());
+//			      model.addAttribute("name", video.getLikes());
+//			      model.addAttribute("name", video.getDislikes());
+//			      model.addAttribute("name", video.getThumbnail());
+//			      model.addAttribute("name", video.getUploadDate());
+//			      model.addAttribute("name", video.getUploader());
 			      
-			      return "uploadFinalization";
+			      return mav;
 			   }
-
 
 }
