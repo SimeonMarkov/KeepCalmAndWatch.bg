1ec85b977f2aeabefbc6c05c523ec1437b40de85
PavelGyurov <pavel.gjurov@gmail.com>
2016-04-03 11:03:06 +0300

    added encodePassword method

--- KeepCalmAndWatch/src/main/java/com/example/controller/RegistrationLoginController.java
+++ KeepCalmAndWatch/src/main/java/com/example/controller/RegistrationLoginController.java
@@ -3,7 +3,9 @@ package com.example.controller;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Base64;
 
+import org.bouncycastle.jcajce.provider.digest.SHA3.DigestSHA3;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -21,56 +23,62 @@ import com.example.model.dao.DBUserDAO;
 @Controller
 public class RegistrationLoginController {
 
-	@RequestMapping(value="/index", method = RequestMethod.GET)
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String sayHello(Model model) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		DBUserDAO userJDBCTemplate = 
-			      (DBUserDAO)context.getBean("DBUserDAO");
+		DBUserDAO userJDBCTemplate = (DBUserDAO) context.getBean("DBUserDAO");
 		User user = userJDBCTemplate.getUser("nikola");
-	      System.out.print("Channel name : " + user.getChannelName() );
-	      System.out.print(", Email : " + user.getEmail() );
-	      System.out.println(", Registered on : " + user.getRegistrationDate());
-	    model.addAttribute(user);
-	    return "test";
-	}	
-	
-	@RequestMapping(value="/register", method = RequestMethod.GET)
+		System.out.print("Channel name : " + user.getChannelName());
+		System.out.print(", Email : " + user.getEmail());
+		System.out.println(", Registered on : " + user.getRegistrationDate());
+		model.addAttribute(user);
+		return "test";
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public ModelAndView register() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-	
+
 		return new ModelAndView("register", "command", new User());
 	}
+
+	@RequestMapping(value = "/registerUser", method = RequestMethod.POST)
+	public String registerUser(@ModelAttribute("SpringWeb") User user, ModelMap model,
+			final RedirectAttributes redirectAttributes) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		user.setRegistrationDate(Date.valueOf(LocalDate.now()));
+		DBUserDAO dbUserDao = (DBUserDAO) context.getBean("DBUserDAO");
+		String fail = "";
+
+		if (dbUserDao.emailExist(user.getEmail())) {
+			fail = "Имейл адресът е вече зает!";
+			redirectAttributes.addFlashAttribute("fail", fail);
+			return "redirect:/register";
+		} else if (dbUserDao.channelExist(user.getChannelName())) {
+			fail = "Канал с такова име вече съществува!";
+			redirectAttributes.addFlashAttribute("fail", fail);
+			return "redirect:/register";
+		} else if (dbUserDao.userExist(user.getUsername())) {
+			fail = "Потребителското име е вече заето!";
+			redirectAttributes.addFlashAttribute("fail", fail);
+			return "redirect:/register";
+		} else if (user.getPassword().length() < 6) {
+			fail = "Паролата съдържа по-малко от 6 символа!";
+			redirectAttributes.addFlashAttribute("fail", fail);
+			return "redirect:/register";
+		} else {
+			dbUserDao.addUser(user);
+			model.addAttribute(user);
+			return "test";
+		}
+	}
 	
-
-	  @RequestMapping(value = "/registerUser", method = RequestMethod.POST)
-	   public String registerUser(@ModelAttribute("SpringWeb")User user, ModelMap model, final RedirectAttributes redirectAttributes) {
-		  ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		  user.setRegistrationDate(Date.valueOf(LocalDate.now()));
-		  DBUserDAO dbUserDao = (DBUserDAO)context.getBean("DBUserDAO");
-		  String fail = "";
-		  
-		  
-		  if(dbUserDao.emailExist(user.getEmail())){
-			  fail = "Имейл адресът е вече зает!";
-			  redirectAttributes.addFlashAttribute("fail", fail);
-			  return "redirect:/register";
-		  }else if(dbUserDao.channelExist(user.getChannelName())){
-			  fail = "Канал с такова име вече съществува!";
-			  redirectAttributes.addFlashAttribute("fail", fail);
-			  return "redirect:/register";
-		  }else if(dbUserDao.userExist(user.getUsername())){
-			  fail = "Потребителското име е вече заето!";
-			  redirectAttributes.addFlashAttribute("fail", fail);
-			  return "redirect:/register";
-		  } else if(user.getPassword().length()<6){
-			  fail = "Паролата съдържа по-малко от 6 символа!";
-			  redirectAttributes.addFlashAttribute("fail", fail);
-			  return "redirect:/register";
-		  }else {
-		  dbUserDao.addUser(user);
-		  model.addAttribute(user);
-	      return "test";
-	      }
-	   }
+	
+	
+	private String encodePassword(String passwod){
+		DigestSHA3 passdigesst = new DigestSHA3(256);
+		passdigesst.update(passwod.getBytes());
+		byte[] digest = passdigesst.digest();
+		return Base64.getEncoder().encodeToString(digest);		//encodes to base64 string to fit in 45 character restriction
+	}
 }
-
