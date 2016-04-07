cd775bb0ac016604a9a05fe2ade5050ff120193c
Simeon Markov <simeon.markov1994@gmail.com>
2016-04-07 17:04:38 +0300

    back end validation for upload form

--- KeepCalmAndWatch/src/main/java/com/example/controller/UploadController.java
+++ KeepCalmAndWatch/src/main/java/com/example/controller/UploadController.java
@@ -4,10 +4,10 @@ import java.io.*;
 import java.sql.Date;
 import java.time.LocalDate;
 
-
 import javax.servlet.http.HttpSession;
 import javax.servlet.annotation.MultipartConfig;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -40,82 +40,109 @@ import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 @RequestMapping("/upload")
 @SessionAttributes("LoggedUser")
 public class UploadController {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String goToUpload(ModelMap modelMap, HttpSession session){
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		DBUserDAO userJDBCTemplate = 
-			      (DBUserDAO)context.getBean("DBUserDAO");
-		if(!(session.getAttribute("LoggedUser") != null)){
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String goToUpload(ModelMap modelMap, HttpSession session) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"beans.xml");
+		DBUserDAO userJDBCTemplate = (DBUserDAO) context.getBean("DBUserDAO");
+		if (!(session.getAttribute("LoggedUser") != null)) {
 			return "redirect:/login";
 		}
 		return "upload";
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView confirmUpload(ModelMap model, @RequestParam("title") String title,
-			@RequestParam("description") String description,  @RequestParam("videoPath") MultipartFile file, 
-			 @RequestParam("thumbnail") MultipartFile thumbnail) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+	public ModelAndView confirmUpload(ModelMap model,
+			@RequestParam("title") String title,
+			@RequestParam("description") String description,
+			@RequestParam("videoPath") MultipartFile file,
+			@RequestParam("thumbnail") MultipartFile thumbnail) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"beans.xml");
 		ModelAndView mav = new ModelAndView("uploadFinalization");
-		
-		
-		DBVideoDAO videoJDBCTemplate = (DBVideoDAO) context.getBean("DBVideoDAO");
-		Video video = new Video();
-		//set amazon username and pass
-		AWSCredentials credentials = new BasicAWSCredentials("AKIAIDEAOQSKMINEQSVA", "o94Ozi37icf6+HoROskITlkAvdwRdphYXsPmrya4"); 
-		AmazonS3Client s3client = new AmazonS3Client(credentials);
-			
-		//amazon folder
-		String bucketName = "keep-calm-videos";
-		s3client.createBucket(bucketName);
-		
-		File convFile = new File(file.hashCode() + file.getOriginalFilename());
-		File convThumbnail = new File(file.hashCode() + thumbnail.getOriginalFilename());
-		try {
-			//converts to files in order to upload to amazon s3
-			convFile.createNewFile();
-			FileOutputStream fos = new FileOutputStream(convFile); 
-		    fos.write(file.getBytes());
-		    fos.close(); 
-		} catch (IOException e1) {
-			System.out.println("File could not be created");
-			e1.printStackTrace();
+
+		if ((FilenameUtils.getExtension(file.getOriginalFilename()).equals(
+				"mp4") && FilenameUtils.getExtension(
+				thumbnail.getOriginalFilename()).equals("jpg"))
+				|| (FilenameUtils.getExtension(file.getOriginalFilename())
+						.equals("mp4") && FilenameUtils.getExtension(
+						thumbnail.getOriginalFilename()).equals("png"))
+				|| (FilenameUtils.getExtension(file.getOriginalFilename())
+						.equals("ogv") && FilenameUtils.getExtension(
+						thumbnail.getOriginalFilename()).equals("jpg"))
+				|| (FilenameUtils.getExtension(file.getOriginalFilename())
+						.equals("ogv") && FilenameUtils.getExtension(
+						thumbnail.getOriginalFilename()).equals("png"))) {
+			DBVideoDAO videoJDBCTemplate = (DBVideoDAO) context
+					.getBean("DBVideoDAO");
+			Video video = new Video();
+			// set amazon username and pass
+			AWSCredentials credentials = new BasicAWSCredentials(
+					"AKIAIDEAOQSKMINEQSVA",
+					"o94Ozi37icf6+HoROskITlkAvdwRdphYXsPmrya4");
+			AmazonS3Client s3client = new AmazonS3Client(credentials);
+
+			// amazon folder
+			String bucketName = "keep-calm-videos";
+			s3client.createBucket(bucketName);
+
+			File convFile = new File(file.hashCode()
+					+ file.getOriginalFilename());
+			File convThumbnail = new File(file.hashCode()
+					+ thumbnail.getOriginalFilename());
+			try {
+				// converts to files in order to upload to amazon s3
+				convFile.createNewFile();
+				FileOutputStream fos = new FileOutputStream(convFile);
+				fos.write(file.getBytes());
+				fos.close();
+			} catch (IOException e1) {
+				System.out.println("File could not be created");
+				e1.printStackTrace();
+			}
+
+			s3client.putObject(new PutObjectRequest(bucketName, convFile
+					.getName(), convFile)
+					.withCannedAcl(CannedAccessControlList.PublicRead));
+
+			String videoPath = s3client.getResourceUrl(bucketName,
+					convFile.getName());
+			String thumbnailPath = s3client.getResourceUrl(bucketName,
+					convThumbnail.getName());
+
+			video.setTitle(title);
+			video.setDescription(description);
+			video.setPath(videoPath);
+			video.setViews(0);
+			video.setLikes(0);
+			video.setDislikes(0);
+			try {
+				video.setThumbnail(Base64.encode(thumbnail.getBytes()));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			video.setUploadDate(Date.valueOf(LocalDate.now()));
+			User user = (User) model.get("LoggedUser");
+			video.setUploader((User) model.get("LoggedUser"));
+
+			videoJDBCTemplate.addVideo(video);
+			mav.addObject(
+					"message",
+					((User) model.get("LoggedUser")).getChannelName()
+							+ ",Вие успешно качихте клип, намиращ се в директорията "
+							+ "path" + ", който е със заглавие: " + title
+							+ " и описание: " + description + " на дата "
+							+ video.getUploadDate());
+
 		}
-		
-		s3client.putObject(new PutObjectRequest(bucketName, convFile.getName(), convFile).withCannedAcl(CannedAccessControlList.PublicRead));
-		
-		
-		String videoPath = s3client.getResourceUrl(bucketName, convFile.getName());
-		String thumbnailPath = s3client.getResourceUrl(bucketName, convThumbnail.getName());
-		
-		video.setTitle(title);
-		video.setDescription(description);
-		video.setPath(videoPath);
-		video.setViews(0);
-		video.setLikes(0);
-		video.setDislikes(0);
-		try {
-			video.setThumbnail(Base64.encode(thumbnail.getBytes()));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		else {
+			System.out.println("You cannot proceed with the upload!!!!!!!!!!!!!!");
+			System.out.println(FilenameUtils.getExtension(file.getOriginalFilename()));
+			System.out.println(FilenameUtils.getExtension(thumbnail.getOriginalFilename()));
 		}
-		video.setUploadDate(Date.valueOf(LocalDate.now()));
-		User user = (User) model.get("LoggedUser");
-		video.setUploader((User) model.get("LoggedUser")); 
-		
-		videoJDBCTemplate.addVideo(video);
-		mav.addObject("message",
-				((User) model.get("LoggedUser")).getChannelName()
-						+ ",Вие успешно качихте клип, намиращ се в директорията " + "path" + ", който е със заглавие: "
-						+ title + " и описание: " + description + " на дата " + video.getUploadDate());
-
-
-
 		return mav;
 	}
-	
-	
-	
+
 }
